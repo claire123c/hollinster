4276e3be7cfeb71ea4ad59efdbdd6bcdf6499bfb
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-08 12:24:45 -0700

    Added test for expandable

--- client/src/client-tests/overview-tests/Overview.test.js
+++ client/src/client-tests/overview-tests/Overview.test.js
@@ -8,14 +8,13 @@ import sampleData from '../../components/overview/sampleData.js';
 
 import Overview from '../../components/overview/Overview.jsx';
 
-jest.mock('axios');
 
 //API tests need to be edited
-describe('API Calls', async () => {
+describe('API Calls', () => {
   it('should get product styles', () => {
-    // const OverviewComp = render(<Overview />);
-    // const gallery = document.querySelector('.defaultview');
-    // expect(gallery.src).toBe(undefined);
+    const OverviewComp = render(<Overview />);
+    const gallery = document.querySelector('.defaultview');
+    expect(gallery.src).toBe(undefined);
     // const resp = sampleData;
     // axios.get.mockResolvedValue(resp);
 
