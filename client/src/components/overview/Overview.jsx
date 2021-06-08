1acdd43de3fa2730140b44b7e90aca080e884725
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-07 17:26:03 -0700

    Added get request for products

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import Gallery from './ImgGallery/Gallery.jsx';
+import axios from 'axios';
 
+import Gallery from './ImgGallery/Gallery.jsx';
 import sampleData from './sampleData.js';
 
 function Overview() {
+  const [productNum] = useState('25167');
+  const getProduct = () => {
+    axios.get(`/products/${productNum}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
     <>
       <Gallery styles={sampleData.results} />
