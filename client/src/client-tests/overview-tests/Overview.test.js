865fa81961509f02c2fae8d02ebc7dc475efa82f
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-08 12:01:41 -0700

    worked on API test a bit

--- client/src/client-tests/overview-tests/Overview.test.js
+++ client/src/client-tests/overview-tests/Overview.test.js
@@ -4,14 +4,24 @@
 import React from 'react';
 import {cleanup, fireEvent, render} from '@testing-library/react';
 import axios from 'axios';
+import sampleData from '../../components/overview/sampleData.js';
 
 import Overview from '../../components/overview/Overview.jsx';
 
+jest.mock('axios');
+
 //API tests need to be edited
-describe('API Calls', () => {
+describe('API Calls', async () => {
   it('should get product styles', () => {
-    const OverviewComp = render(<Overview />);
-    const gallery = document.querySelector('.defaultview');
-    expect(gallery.src).toBe(undefined);
+    // const OverviewComp = render(<Overview />);
+    // const gallery = document.querySelector('.defaultview');
+    // expect(gallery.src).toBe(undefined);
+    // const resp = sampleData;
+    // axios.get.mockResolvedValue(resp);
+
+    // return Overview.all()
+    //   .then((data) => (
+    //     expect(data).toEqual()
+    //   ));
   });
-})
+});
