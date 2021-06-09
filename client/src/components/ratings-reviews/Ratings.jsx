95faf3ee8b70df9a7f677cf4fd3fc0f251610101
Hollin Wakefield <45081384+hollinwakefield@users.noreply.github.com>
2021-06-09 05:24:57 -0700

    Ignored compiled asset

--- client/src/components/ratings-reviews/Ratings.jsx
+++ client/src/components/ratings-reviews/Ratings.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import axios from 'axios';
+// Must import product_ID for successful git request
+//
 
-function Ratings() {
-  return (
-    <>
-      <p>Ratings will appear here.</p>
-    </>
-  );
+class Ratings extends React.Component {
+  componentDidMount() {
+    axios.get('').then((response) => {
+
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <p>Ratings will appear here.</p>
+      </>
+    );
+  }
 }
+
+// function Ratings() {
+//   return (
+
+//   );
+// }
 
 export default Ratings;
