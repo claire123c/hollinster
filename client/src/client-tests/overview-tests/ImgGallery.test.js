fd68014053a3f3a2049887e5dbf81816b7068839
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-08 22:24:01 -0700

    fixed left arrow test

--- client/src/client-tests/overview-tests/ImgGallery.test.js
+++ client/src/client-tests/overview-tests/ImgGallery.test.js
@@ -40,29 +40,29 @@ describe('Right Arrow', () => {
   });
 });
 
-xdescribe('Left Arrow', () => {
+describe('Left Arrow', () => {
   test('Left Arrow changes image after click', () => {
     const LargeComp = render(<Large defaultStyle={sampleData.results[0]} />);
     const rightArrow = LargeComp.getByTestId('rightArrowImgGallery');
     const leftArrow = LargeComp.getByTestId('leftArrowImgGallery');
-    const currentImage = LargeComp.getByAltText(sampleData.results[0].name);
+    const currentImage = document.querySelector('.defaultview');
 
     fireEvent.click(rightArrow);
-    const previousImgSrc = currentImage.src;
+    const previousAttr = currentImage.getAttribute('src');
     fireEvent.click(leftArrow);
 
-    expect(currentImage.src).not.toBe(previousImgSrc);
+    expect(currentImage.getAttribute('src')).not.toBe(previousAttr);
   });
 
   test('Left Arrow does not change image when at first image', () => {
     const LargeComp = render(<Large defaultStyle={sampleData.results[0]} />);
     const leftArrow = LargeComp.getByTestId('leftArrowImgGallery');
-    const currentImage = LargeComp.getByAltText(sampleData.results[0].name);
-    const previousImgSrc = currentImage.src;
+    const currentImage = document.querySelector('.defaultview');
+    const previousAttr = currentImage.getAttribute('src');
 
     fireEvent.click(leftArrow);
 
-    expect(currentImage.src).toBe(previousImgSrc);
+    expect(currentImage.getAttribute('src')).toBe(previousAttr);
   });
 });
 
