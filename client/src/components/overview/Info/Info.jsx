51c1578de7560c8c980fd1740f02c44c15813cef
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-11 14:47:41 -0700

    Added middleware to get rid of samesite error

--- client/src/components/overview/Info/Info.jsx
+++ client/src/components/overview/Info/Info.jsx
@@ -26,15 +26,17 @@ function Info({ productInfo, styles, reviews }) {
         {sale_price ? (
           <div>
             <SalePrice className="sales">
-              ${sale_price}
+              $
+              {sale_price}
             </SalePrice>
             <Original className="original">
-              ${original_price}
+              $
+              {original_price}
             </Original>
           </div>
         ) : original_price}
       </div>
-      <div className="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">Share</a></div>
+      <div className="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore" rel="noreferrer">Share</a></div>
     </div>
   );
 }
