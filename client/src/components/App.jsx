6ddb00f737b2ed80b8ad52445fc0cd636aa08ecb
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-11 11:19:42 -0700

    Added read reviews to hide if no review

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,7 @@
 // imports the react module installed in node_modules
 import React, { useState } from 'react';
 // import axios from 'axios';
-// import Overview from './overview/Overview.jsx';
+import Overview from './overview/Overview.jsx';
 // import Related from './related/Related.jsx'
 
 const App = (props) => {
@@ -11,7 +11,7 @@ const App = (props) => {
     <>
       <h1 style={{ fontFamily: 'Staatliches' }}>HOLLINSTER established 1991</h1>
       <button onClick={() => {setTest(!test)}}>{test.toString()}</button>
-      {/* <Overview /> */}
+      <Overview />
       {/* <Related /> */}
     </>
   );
