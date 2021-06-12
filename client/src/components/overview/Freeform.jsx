e8c8756425d61c917d9d65c0975971d323a5b232
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-11 17:15:21 -0700

    Added list styled component

--- client/src/components/overview/Freeform.jsx
+++ client/src/components/overview/Freeform.jsx
@@ -2,12 +2,27 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+const List = styled.ul`
+  list-style: none;
+  margin-left: 0;
+  padding-left: 1em;
+  text-indent: -1em;
+`;
 function Freeform({ productInfo }) {
-  const { name, category } = productInfo;
-  console.log(productInfo);
+  const { slogan, description, features } = productInfo;
+  console.log(features);
+
 
   return (
-    <div>hihi</div>
+    <div>
+      <div>{slogan}</div>
+      <div>{description}</div>
+      <List>
+        <li>hi</li>
+        <li>sadf</li>
+        <li>asdfasdfsadf</li>
+      </List>
+    </div>
   );
 }
 
