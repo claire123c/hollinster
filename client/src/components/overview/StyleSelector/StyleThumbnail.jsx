8aded9bc6f3c9dca8211d5e7f8a94d8efbf0f156
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-15 09:58:55 -0700

    Got rid of extra checkmarks

--- client/src/components/overview/StyleSelector/StyleThumbnail.jsx
+++ client/src/components/overview/StyleSelector/StyleThumbnail.jsx
@@ -40,7 +40,7 @@ function StyleThumbnail({ style, current, changeStyle }) {
   return (
     <StyleThumbComp>
       <StylePhoto src={thumbnail_url} alt={name} onClick={onClickStyle} className="style-photo" />
-      {style.style_id === current ? <CheckMark src="./assets/check-mark-black-outline.png" alt="checkmark" /> : <CheckMark />}
+      {style.style_id === current ? <CheckMark src="./assets/check-mark-black-outline.png" alt="checkmark" /> : <></>}
     </StyleThumbComp>
   );
 }
