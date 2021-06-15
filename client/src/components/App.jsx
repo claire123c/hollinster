dd706a1b33181eedae8bb5143bfae16e214b04a6
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-15 10:19:22 -0700

    commented out overview

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,7 @@
 // imports the react module installed in node_modules
 import React, { useState } from 'react';
 // import axios from 'axios';
-import Overview from './overview/Overview.jsx';
+// import Overview from './overview/Overview.jsx';
 // import Related from './related/Related.jsx';
 // import Question from './question-answer/Question.jsx';
 // import RatingsReviews from './ratings-reviews/ratings-reviews.jsx';
@@ -15,7 +15,7 @@ const App = (props) => {
       <h1 style={{ fontFamily: 'Staatliches' }}>HOLLINSTER established 1991</h1>
       <button onClick={() => {setTest(!test)}}>{test.toString()}</button>
 
-      <Overview />
+      {/* <Overview /> */}
       {/* <Question /> */}
       {/* <Related /> */}
       {/* <RatingsReviews productID={productID} /> */}
