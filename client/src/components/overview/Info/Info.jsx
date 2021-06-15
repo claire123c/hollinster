8d8cfc36982c29ef95eae3f48ec2a1a849c5730b
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-15 15:44:28 -0700

    removed extra text for twitter button

--- client/src/components/overview/Info/Info.jsx
+++ client/src/components/overview/Info/Info.jsx
@@ -76,7 +76,7 @@ function Info({
       </div>
       <Social>
         <div className="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore" rel="noreferrer">Share</a></div>
-        <a href="https://twitter.com/Cpak90?ref_src=twsrc%5Etfw" className="twitter-follow-button" data-show-count="false">Follow @Cpak90</a>
+        <a href="https://twitter.com/Cpak90?ref_src=twsrc%5Etfw" className="twitter-follow-button" data-show-count="false">Follow</a>
         <Pin className="pinterest-save-button">
           <a href="https://www.pinterest.com/pin/create/button/" data-pin-do="buttonBookmark" />
         </Pin>
