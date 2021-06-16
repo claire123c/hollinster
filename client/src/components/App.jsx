d058b58a818fe93e3d7cb5ef6bf3f4dde13c9b69
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-16 14:23:39 -0700

    merge

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,25 +2,17 @@
 import React, { useState } from 'react';
 // import axios from 'axios';
 import Overview from './overview/Overview.jsx';
-<<<<<<< HEAD
-// import Related from './related/Related.jsx';
-=======
 import Related from './related/Related.jsx';
->>>>>>> main
 // import Question from './question-answer/Question.jsx';
 // import RatingsReviews from './ratings-reviews/ratings-reviews.jsx';
 
 const App = (props) => {
   const [test, setTest] = useState(true);
-<<<<<<< HEAD
-  const productID = 25174;
-=======
   const [productID, setProductID] = useState(25167);
 
   const switchProduct = () => {
     setProductID(product);
   };
->>>>>>> main
 
   return (
     <>
@@ -29,11 +21,7 @@ const App = (props) => {
 
       <Overview productID={productID} />
       {/* <Question /> */}
-<<<<<<< HEAD
-      {/* <Related productID={productID} /> */}
-=======
       <Related productID={productID} setProductID={setProductID} switchProduct={switchProduct} />
->>>>>>> main
       {/* <RatingsReviews productID={productID} /> */}
     </>
   );
