ea58d246dbdfb5ca0f7a30f135e614b33ac48d74
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-17 17:12:16 -0700

    uncommented

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,9 +3,9 @@ import React, { useState } from 'react';
 // import axios from 'axios';
 import styled from 'styled-components';
 import Overview from './overview/Overview.jsx';
-// import Related from './related/Related.jsx';
-// import Question from './question-answer/Question.jsx';
-// import RatingsReviews from './ratings-reviews/ratings-reviews.jsx';
+import Related from './related/Related.jsx';
+import Question from './question-answer/Question.jsx';
+import RatingsReviews from './ratings-reviews/ratings-reviews.jsx';
 
 const LogoBar = styled.h1`
   font-family: 'Staatliches';
@@ -58,9 +58,9 @@ const App = (props) => {
       </LogoBar>
       <button type="button" onClick={() => { setTest(!test); }}>{test.toString()}</button>
       <Overview productID={productID} />
-      {/* <Question />
+      <Question />
       <Related productID={productID} setProductID={setProductID} switchProduct={switchProduct} />
-      <RatingsReviews productID={productID} /> */}
+      <RatingsReviews productID={productID} />
     </>
   );
 };
