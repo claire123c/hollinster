0acb2a21a967ee031caed441535bb9d9cc8edf3a
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-06-17 18:12:12 -0700

    Added dropdown for quantity

--- client/src/components/overview/AddCart/Quantity.jsx
+++ client/src/components/overview/AddCart/Quantity.jsx
@@ -9,13 +9,63 @@ const QuantityBox = styled.div`
 
 `;
 
-const QuantityDropDown = styled.select`
+const ContainerQ = styled.div`
+  position: relative;
+  width: 50%;
+  height: 100%;
+  padding-bottom: 5%;
+`;
 
+const HeaderContQ = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  font-weight: bold;
+`;
+const QuantHeader = styled.li`
+
+`;
+
+const QuantityDropDown = styled.div`
+  border: 1px solid black;
+  padding: 0;
+  margin-top: 8%;
+  z-index: 2;
+  background-color: #F5F4F2;
+  border: 1px solid rgba(0, 0, 0, 0.04);
+  box-shadow: 0 16px 24px 2px rgba(0, 0, 0, 0.14);
+  overflow: scroll;
+  position: absolute;
+  width: 90%;
+  height: 300px;
+`;
+
+const QuantityOptions = styled.li`
+  list-style: none;
+  padding: 3%;
+  margin: 1%;
+  width: 90%;
+  border: none;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.14);
+  }
+`;
+
+const ArrowImgQ = styled.img`
+  height: 15px;
+  margin-left: 40%;
 `;
 
 function Quantity({ currentSizeObj }) {
   const { quantity } = currentSizeObj;
   const [isOpenQ, setIsOpenQ] = useState(false);
+  let imageSource = './assets/down-chevron.png';
+
+  if (!isOpenQ) {
+    imageSource = './assets/down-chevron.png';
+  } else {
+    imageSource = './assets/up-chevron.png';
+  }
   const getQuantities = () => {
     let num;
     const numArray = [];
@@ -27,7 +77,7 @@ function Quantity({ currentSizeObj }) {
     let i = 1;
     while (i <= num) {
       numArray.push(
-        <option value={i} key={i}>{i}</option>,
+        <QuantityOptions value={i} key={i}>{i}</QuantityOptions>,
       );
       i += 1;
     }
@@ -41,11 +91,35 @@ function Quantity({ currentSizeObj }) {
       </QuantityBox>
     );
   }
+
+  if (!isOpenQ) {
+    return (
+      <QuantityBox>
+        <ContainerQ>
+          <HeaderContQ>
+            <QuantHeader onClick={() => { setIsOpenQ(!isOpenQ); }}>
+              SELECT QUANTITY
+              <ArrowImgQ src={imageSource} alt="arrows" />
+            </QuantHeader>
+          </HeaderContQ>
+        </ContainerQ>
+      </QuantityBox>
+    );
+  }
+
   return (
     <QuantityBox>
-      <QuantityDropDown className="quantity-selector">
-        {getQuantities()}
-      </QuantityDropDown>
+      <ContainerQ>
+        <HeaderContQ>
+          <QuantHeader onClick={() => { setIsOpenQ(!isOpenQ); }}>
+            SELECT QUANTITY
+            <ArrowImgQ src={imageSource} alt="arrows" />
+          </QuantHeader>
+        </HeaderContQ>
+        <QuantityDropDown className="quantity-selector">
+          {getQuantities()}
+        </QuantityDropDown>
+      </ContainerQ>
     </QuantityBox>
   );
 }
