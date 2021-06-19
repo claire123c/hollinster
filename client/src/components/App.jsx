96c8a63075e70a556af2844a10595282756999e2
John Yasis <johnamielyasis@gmail.com>
2021-06-19 14:50:13 -0700

    successfully resolving post request for answer form

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 // import Overview from './overview/Overview.jsx';
 // import Related from './related/Related.jsx';
-// import Question from './question-answer/Question.jsx';
+import Question from './question-answer/Question.jsx';
 // import RatingsReviews from './ratings-reviews/ratings-reviews.jsx';
 
 window.onbeforeunload = function () {
@@ -115,7 +115,7 @@ const App = (props) => {
       {theme ? <LightButton type="button" onClick={onClickTheme}>Go Dark</LightButton> : <DarkButton type="button" onClick={onClickTheme}>Light it up!</DarkButton>}
       {/* <Overview productID={productID} /> */}
       {/* <Related productID={productID} /> */}
-      {/* <Question productID={productID} /> */}
+      <Question productID={productID} />
       {/* <RatingsReviews productID={productID} /> */}
     </Body>
   );
