dec1b59ed17f25951dd2e8cb1800698b8e0f72d9
Hollin Wakefield <45081384+hollinwakefield@users.noreply.github.com>
2021-06-19 15:14:48 -0700

    Merge prep

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -125,6 +125,4 @@ const App = (props) => {
   );
 };
 
-// attribute free icon flaticon at bottom of website: <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-
 export default App;
