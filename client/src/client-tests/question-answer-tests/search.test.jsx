dd2fc1771effde15f20a63140cfb4fc56584669c
John Yasis <johnamielyasis@gmail.com>
2021-06-18 19:54:48 -0700

    failing circle CI tests d/t incomplete test

--- client/src/client-tests/question-answer-tests/search.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {cleanup, fireEvent, render} from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Search from '../../components/question-answer/Search.jsx';
-
-// does search bar render to screen
-// should render proper placeholder message
-// input typed should appear on the search bar
